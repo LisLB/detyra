c1ecc73232a5eb2b7ea61135fac2ef12495c70a7
LisLB <lisbakija@gmail.com>
2025-02-03 18:13:44 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var cmimi = 10;
+
+var kilogram = prompt("Sa kilogram molla deshironi te bleni?");
+if (kilogrami > 0){
+    var totali = cmimi * kilogrami;
+    console.log(totali);
+} else{
+    console.log("Pishpirik jepe nje vlere ma nalt se 0");
+}
+ console.log(totali)
---
+++ script.js
@@ -0,0 +1,9 @@
+var emri = "Lisi"; // String
+var mbiemri ="bakija"; // String
+var work = false; // Boolean
+var mosha = 12; // integer
+var gjatesia = 1.64; // float
+console.log("Une jam:" + " " + emri + " " + mbiemri)
+console.log("A puno une:" + work)
+console.log("Mosha ime eshte:" + mosha)
+console.log("Gjatesia ime eshte:" + gjatesia)
