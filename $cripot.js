840344602a188510ae41fa013b1e0424c9ddd604
LisLB <lisbakija@gmail.com>
2025-02-05 18:14:05 +0100

    Add files via upload

---
+++ $cripot.js
@@ -0,0 +1,30 @@
+// var number = prompt("Shkruani nje numer");
+
+var matematika = 1;
+var fizike = 1;
+var muzike = 1;
+var art = 1;
+var biologji = 1;
+
+var rezultati = matematika+fizike+muzike+art+biologji / 5;
+
+if(rezultati > 4.5){
+    console.log("Ti je i shkelqyeshem")
+}
+if(rezultati < 4.5){
+    console.log("Ti je budall sikur arsimtarja jeme e fizikes:)")
+}
+
+// if(rezultati > 15){
+//     alert("Ti je i shkeqyeshem")
+// }
+// if(number > 0){
+//     if(number % 2 == 0){
+//         alert("Numeri eshte qift")
+//     }else if(number % 2 == 1){
+//         alert("Numeri eshte tek")
+//     }
+// }
+// else {
+//     console.log("Numeri eshte negativ");
+// }
