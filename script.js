f3165a8318b9a2065302184037ddc7c816ffb8aa
LisLB <lisbakija@gmail.com>
2025-03-12 18:57:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-var emri = "Lisi"; // String
-var mbiemri ="bakija"; // String
-var work = false; // Boolean
-var mosha = 12; // integer
-var gjatesia = 1.64; // float
-console.log("Une jam:" + " " + emri + " " + mbiemri)
-console.log("A puno une:" + work)
-console.log("Mosha ime eshte:" + mosha)
-console.log("Gjatesia ime eshte:" + gjatesia)
+function addTask() {
+var taskInput = document.getElementById("taskInput");
+var tasktext = taskInput.value ;
+
+if (tasktext === '') {
+    alert("Please write a task");
+    return;
+    
+}
+
+// .value e merr vleren
+// .innerHTML = e shton ose modifikon vleren
+
+// .createElement
+
+// pe e krijojme nje element li
+// pe marrum veleren qe e ka shenu po ja bashkangjitur nje buton "X"
+var li = document.createElement("li");
+li.innerHTML = tasktext + '<button onclick="removeTask(this)">X</button>';
+
+// apendchild e shton elementin e krijuar ne fund te nje elementi tjeter
+document.getElementById("taskList").appendChild(li);
+taskInput.value = '';
+
+}
+
+function removeTask() {
+    var li = this.parentElement;
+    li.remove();
+}
